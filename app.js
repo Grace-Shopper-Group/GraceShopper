a076ac2438905730867e76155dbec7ce94404324
cardonandrew <cardonandrew0@gmail.com>
2023-01-07 11:55:07 -0700

    feat: added middleware and router to server/still buggy

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const http = require("http")
 const express = require("express");
 const app = express();
+const router = require('./api');
 const server = http.createServer(app)
+const { client } = require('./db');
+const morgan = require('morgan');
+app.use(morgan('dev'));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+server.use(express.json())
+
+
+client.connect();
+
+server.use('/api', (req, res, next) => {
+    console.log("A request was made to /api");
+    next();
+  });
+
+server.use('/api', router);
+
+server.get("/", (req, res) => res.send("Hello World!"));
 
 const PORT = 1337;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
