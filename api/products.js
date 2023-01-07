0b33b08f761ae098dd267e3bedce961b2bfd7204
cardonandrew <cardonandrew0@gmail.com>
2023-01-07 09:17:56 -0700

    feat: API GET and POST functions, DB update and delete functions

--- api/products.js
+++ api/products.js
@@ -2,9 +2,27 @@ const express = require('express');
 const productsRouter = express.Router();
 const { getAllProducts, createProduct } = require('../db')
 
-productsRouter.get('/products', async (req, res, next) => {
+productsRouter.get('/products', async (req, res) => {
     const allProducts = await getAllProducts()
-        if(allProducts){console.log('allProducts', allProducts)}
+        
+        if (allProducts) {
+            console.log('allProducts', allProducts)
+        }
     res.send(allProducts)
+})
 
+productsRouter.post('/products', async (req, res) => {
+    const { brand, description, category, price, img } = req.body
+
+    const newProduct = await createProduct(brand, description, category, price, img)
+
+    res.send(newProduct)
+})
+
+productsRouter.patch('/products:productId', async (req, res) => {
+  
+})
+
+productsRouter.delete('/products:productId', async (req, res) => {
+  
 })
