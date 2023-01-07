5e561dd983e7a87647e9cfe5934d03dd3ea5843f
cardonandrew <cardonandrew0@gmail.com>
2023-01-07 09:23:05 -0700

    fix: changed api routes

--- api/products.js
+++ api/products.js
@@ -1,17 +1,19 @@
 const express = require('express');
+const { nextTick } = require('process');
 const productsRouter = express.Router();
 const { getAllProducts, createProduct } = require('../db')
 
-productsRouter.get('/products', async (req, res) => {
+productsRouter.get('/', async (req, res, next) => {
     const allProducts = await getAllProducts()
         
         if (allProducts) {
             console.log('allProducts', allProducts)
         }
     res.send(allProducts)
+    next();
 })
 
-productsRouter.post('/products', async (req, res) => {
+productsRouter.post('/', async (req, res) => {
     const { brand, description, category, price, img } = req.body
 
     const newProduct = await createProduct(brand, description, category, price, img)
@@ -19,10 +21,10 @@ productsRouter.post('/products', async (req, res) => {
     res.send(newProduct)
 })
 
-productsRouter.patch('/products:productId', async (req, res) => {
+productsRouter.patch('/:productId', async (req, res) => {
   
 })
 
-productsRouter.delete('/products:productId', async (req, res) => {
+productsRouter.delete('/:productId', async (req, res) => {
   
 })
