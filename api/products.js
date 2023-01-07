8cf76744e038c41faab4e99c5ff32c62a000a25b
cardonandrew <cardonandrew0@gmail.com>
2023-01-07 11:29:44 -0700

    feat: updateProduct function

--- api/products.js
+++ api/products.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { nextTick } = require('process');
+const {requireUser} = require('./utils');
+const jwt = require('jsonwebtoken');
+const  {JWT_SECRET}= process.env;
 const productsRouter = express.Router();
-const { getAllProducts, createProduct } = require('../db')
+const { getAllProducts, createProduct, deleteProduct, updateProduct } = require('../db')
 
 productsRouter.get('/', async (req, res, next) => {
     const allProducts = await getAllProducts()
@@ -22,9 +24,45 @@ productsRouter.post('/', async (req, res) => {
 })
 
 productsRouter.patch('/:productId', async (req, res) => {
-  
+    const productId = req.params
+    const fields = req.body
+    const user = req.user
+
+    const prefix = 'Bearer ';
+    const auth = req.header('Authorization');
+
+    if (!auth) { 
+        res.status(401)
+        res.send ({
+            error: "Error",
+            name: "Must be logged in Error",
+            message: "You must be logged in to perform this action"
+            });
+        }
+    const token = auth.slice(prefix.length);
+
+    const { id } = jwt.verify(token, JWT_SECRET);
+
+    try {
+        if (user.isAdmin === false){
+
+            res.status(403);
+            res.send({
+                error: "Error",
+                name: "UnauthorizedUserError",
+                message: `User ${req.user.username} is not allowed to update product` 
+        })} else { 
+            const patchedProduct = await updateProduct(productId, fields) 
+            res.send(patchedProduct)
+        }} catch (error){ 
+            console.log(error)
+    }
 })
 
 productsRouter.delete('/:productId', async (req, res) => {
-  
+    const productId = req.params
+
+    const deletedProduct = await deleteProduct(productId)
+
+    res.send(deletedProduct)
 })
