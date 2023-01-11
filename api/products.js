a8b35cefbbff87ec04011a2be7ad56a141c2dd33
cardonandrew <cardonandrew0@gmail.com>
2023-01-10 18:57:31 -0700

    feat: api/products patch method

--- api/products.js
+++ api/products.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const { nextTick } = require("process");
+const { JWT_SECRET } = process.env
 const productsRouter = express.Router();
-const { getAllProducts, createProduct, getUserByUsername } = require("../db");
+const { getAllProducts, createProduct, getUserByUsername, updateProduct } = require("../db");
 
 productsRouter.get("/", async (req, res, next) => {
   const allProducts = await getAllProducts();
@@ -45,10 +45,61 @@ productsRouter.post("/", async (req, res) => {
 });
 
 productsRouter.patch("/:productId", async (req, res) => {
-    const user = req.user
-    const productId = req.params
+    
+    const { brand, description, category, price, img } = req.body;
+    
+    const updateData = { id: req.params.productId };
+    
+    if (brand){updateData.brand = brand};
 
+    if (description){updateData.description = description};
 
+    if (category){updateData.category = category};
+
+    if (price){updateData.price = price};
+    
+    if (img){updateData.img = img};
+
+    const prefix = 'Bearer'
+    const auth = req.header('Authorization')
+    
+    if (!auth){
+        res.status(401)
+        res.send({
+            error: "Error",
+            name: "Token Error",
+            message: "You must be logged in to perform this action"
+        })}
+
+    const token = auth.slice(prefix.length);
+
+    const tokenVerified = jwt.verify(token, JWT_SECRET)
+
+        console.log("token id:", tokenVerified.id)
+
+    try {
+
+        const changedProduct = await updateProduct(req.params.productId, updateData)
+
+        if (!changedProduct) {
+            res.send({
+                name: "Error",
+                error: "Could not update the product",
+                message: `Product ${updateData.id} not found`
+            });
+        } else if (changedProduct.error) {
+            res.send({
+                name: "Error",
+                error: changedProduct.error,
+                message: `An product with name ${req.body.name} already exists`
+            })
+        } else {
+            res.send(changedProduct)
+        }
+
+    } catch (error){
+        next(error)
+    }
 
 
 });
