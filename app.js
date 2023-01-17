c415540be4f0d0b12ce2622d2f1ccd4ef79ab4aa
Bryan Cardon <becard01@yahoo.com>
2023-01-17 07:57:29 -0700

    fix cors 10

--- app.js
+++ app.js
@@ -32,6 +32,11 @@ app.options('*', cors());
 app.use('/api', router) 
 
 router.use('/api', (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
     console.log("A request was made to /api");
     next();
   });
