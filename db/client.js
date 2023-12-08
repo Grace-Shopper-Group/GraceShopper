b4ecdbfc8a53ce71cba0c2b114f1110bedf9e654
cardonandrew <cardonandrew0@gmail.com>
2023-12-07 20:18:54 -0700

    database connection for test

--- db/client.js
+++ db/client.js
@@ -1,10 +1,14 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
-const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/grace-shopper';
+const connectionString =
+  process.env.DATABASE_URL || "https://localhost:5432/grace-shopper";
 
 const client = new Pool({
   connectionString,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
 });
 
 module.exports = client;
